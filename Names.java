9e57fdc0f6733033a45584a0d7f8989801320181
NataliaDAG <143017619+NataliaDAG@users.noreply.github.com>
2023-08-23 13:21:49 -0400

    Names.java

---
+++ Names.java
@@ -0,0 +1,12 @@
+public class Names {
+	
+	public static void main(String[] args) {
+		String[] names = {"Ella", "Carl", "Felicia", "Bob", "Alice", "Donald"};
+		for (String n : names) {
+
+			System.out.println(n);
+
+		}
+	}
+
+}
